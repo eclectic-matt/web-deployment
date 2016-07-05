b0787810a07c9f6acb4e7e13d7ca324db54e5331
Matt <eclecticappdevelopment@gmail.com>
2016-07-05 16:23:58 +0100

    Create pi-calc.php

---
+++ pi-calc.php
@@ -0,0 +1,3 @@
+// The formula is as follows (from https://en.wikipedia.org/wiki/Bailey–Borwein–Plouffe_formula)
+
+$PI = Sum{K=0 -> Infinity} [ 1 / 16^K * ( 4 / 8K + 1 ) - ( 2 / 8K + 4 ) - ( 1 / 8K + 5 ) - ( 1 / 8K + 6) ]
