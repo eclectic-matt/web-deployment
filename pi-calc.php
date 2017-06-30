240f7ca48c678055b57f6740f8776872e7a35c86
Matt <eclecticappdevelopment@gmail.com>
2017-06-30 11:12:56 +0100

    Update pi-calc.php

--- pi-calc.php
+++ pi-calc.php
@@ -1,3 +1,12 @@
 // The formula is as follows (from https://en.wikipedia.org/wiki/Bailey–Borwein–Plouffe_formula)
 
 $PI = Sum{K=0 -> Infinity} [ 1 / 16^K * ( 4 / 8K + 1 ) - ( 2 / 8K + 4 ) - ( 1 / 8K + 5 ) - ( 1 / 8K + 6) ]
+
+$limit = 100;
+$piVal = 0;
+
+for ($k = 0; $k < $limit; $k++){
+  $piVal +=  (1 / (16 ^ $k) * (4 / ((8 * $k) + 1) - (2 / ((8 * $k) + 4)  - ( 1 / (8 * $k) + 5) - (1 / (8 * $k) + 6) )
+}
+                         
+echo "PI calc: " . $piVal;
