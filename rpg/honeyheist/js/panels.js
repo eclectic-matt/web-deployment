dcc0b4fc1869a19c39ec126e561f759b20236bca
Matt <eclecticappdevelopment@gmail.com>
2024-04-13 12:03:01 +0100

    Update js

---
+++ rpg/honeyheist/js/panels.js
@@ -0,0 +1,8 @@
+const openPanel = (panelName) => {
+  var i;
+  var x = document.getElementsByClassName("panel");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  document.getElementById(panelName).style.display = "block";
+}
