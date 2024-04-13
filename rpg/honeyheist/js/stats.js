39c919af06bc73acb3ee97d2f5b202e59a19e56f
Matt <eclecticappdevelopment@gmail.com>
2024-04-13 17:31:41 +0100

    Update stats.js

--- rpg/honeyheist/js/stats.js
+++ rpg/honeyheist/js/stats.js
@@ -1,3 +1,5 @@
+
+//FOR CHARACTER SHEET
 const changeStat = (stat) => {
 
   let bearEl = document.getElementById('bearStat');
@@ -23,4 +25,60 @@ const changeStat = (stat) => {
   }
   criminalEl.innerHTML = criminal;
   bearEl.innerHTML = bear;
+}
+
+//FOR GAME SHEET
+changePlayerStat => (playerId) => {
+  let bearEl = document.getElementById('bearStat' + playerId);
+  let bear = parseInt(bearEl.innerHTML);
+
+  let criminalEl = document.getElementById('criminalStat' + playerId);
+  let criminal = parseInt(criminalEl.innerHTML);
+
+let endTextEl = document.getElementById('endText' + playerId);
+  switch(stat){
+    case 'bear':
+      bear++;
+      criminal--;
+      if(bear === 6){
+        endTextEl.innerHTML = 'BEAR!;
+      }
+    break;
+    case 'criminal':
+      bear--;
+      criminal++;
+      if(criminal === 6){
+        endTextEl.innerHTML = 'CRIM!;
+      }
+    break;
+  }
+  criminalEl.innerHTML = criminal;
+  bearEl.innerHTML = bear;
+}
+
+const addPlayer = () => {
+  let playerTableEl = document.getElementById('playerTable');
+
+  //TOP ROW = 1st SO SUBTRACT 1
+  let newPlayerId = playerTableEl.children.length - 1;
+  
+  let row = document.createElement('tr');
+  
+  let bearStatTd = document.createElement('td');
+
+  let bearStatEl = document.createElement('b');
+  bearStatEl.id = 'bearStat' + newPlayerId;
+  bearStatEl.innerHMTL = '3';
+  bearStatTd.appendChild(bearStatEl);
+  row.appendChil(bearStatTd);
+
+  let bearBtnTd = 
+  let bearBtn = document.createElement('button');
+  bearBtn.innerHTML = '+ Bear';
+  bearBtnTd.appendChild(bearBtn);
+  row.appendChild(bearBtnTd);
+
+  //-----
+
+  playerTableEl.appendChild(row);
 }
