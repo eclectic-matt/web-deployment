b13a277f91322fb6896329e35ed84f3c501e51ae
Matt <eclecticappdevelopment@gmail.com>
2024-04-14 09:27:37 +0100

    Update stats.js

--- rpg/honeyheist/js/stats.js
+++ rpg/honeyheist/js/stats.js
@@ -82,6 +82,7 @@ const addPlayer = () => {
 
   let bearBtnTd = document.createElement('td');
   let bearBtn = document.createElement('button');
+  bearBtn.class = "w3-btn w3-brown";
   bearBtn.innerHTML = '+ Bear';
   bearBtn.onclick = function(){ changePlayerStat(newPlayerId, 'bear')};
   //bearBtn.onclick = "changePlayerStat(" + newPlayerId + ", 'bear')";
@@ -90,6 +91,7 @@ const addPlayer = () => {
 
   let crimBtnTd = document.createElement('td');
   let crimBtn = document.createElement('button');
+  crimBtn.class = "w3-btn w3-grey";
   crimBtn.innerHTML = '+ Crim';
   crimBtn.onclick = function(){ changePlayerStat(newPlayerId, 'criminal') };
   //crimBtn.onclick = "changePlayerStat( " + newPlayerId + ", 'criminal')";
