3f9ba5a4a46b12c649f1c3a2a03a92ce1ee08947
Matt <eclecticappdevelopment@gmail.com>
2024-10-29 16:25:31 +0000

    Update GridGraph.js

--- games/City/GridGraph.js
+++ games/City/GridGraph.js
@@ -1,4 +1,4 @@
-class GridGraph extends Graph 
+class GridGraph extends Graph
 {
   rows = 0;
   cols = 0;
@@ -14,37 +14,56 @@ class GridGraph extends Graph
     let colStreets = Array.fill(colCount);*/
     for (let row = 0; row < rowCount; row++) {
       for (let col = 0; col < colCount; col++) {
+        
+      }
+    }
+
+    //DRAW THE |_ OF A EACH GRID SQUARE |_| 
+    for (let row = 0; row < rowCount; row++) {
+      for (let col = 0; col < colCount; col++) {
+        
+        //All rows/cols - add intersection
         let name = 'grid[' + row + ',' + col + ']';
         let intersection = this.createNode(name);
+        //console.log(name, row, col);
+        
+        //start as bottom right (row,col)
+        //bL = same row, col - 1
+        //tR = prev row, col 
+        //left = bR -> bL
+        //up = bR -> tR
+        //let tL = 'grid[' + row + ',' + (col-1) + ']';
+        
+        let bR = 'grid[' + (row) + ',' + (col) + ']';
+        let tR = 'grid[' + (row-1) + ',' + col + ']';
+        let bL = 'grid[' + (row) + ',' + (col - 1) + ']';
+        
+        //DRAW LEFT AND UP
+        if( (row == 0) && (col == 0) ){
+          //Draw no edges 
+          
+        }else if(row == 0){
+          if(col > 0){
+            //Draw left only 
+            let left = this.createEdge(this.getNodeByName(bL), this.getNodeByName(bR));
+            //console.log('addLeft', row, col, bL, bR);
+            //console.log(row, col,'left', left.id);
+          }
+        }else if(col == 0){
+          if(row > 0){
+            //Draw up only
+            let up = this.createEdge(this.getNodeByName(bR), this.getNodeByName(tR));
+            //console.log('addUp', row, col, bR, tR);
+            //console.log(row, col,'up', up.id);
+          }
+        }else{
+          //BOTH LEFT AND UP
+          let left = this.createEdge(this.getNodeByName(bL), this.getNodeByName(bR));
+          
+          let up = this.createEdge(this.getNodeByName(bR), this.getNodeByName(tR));
+          //console.log(row, col,'up+left', left.id, up.id);
+        }
       }
-    }
-  
-    //DRAW THE |_ OF A EACH GRID SQUARE |_| 
-    for (let row = 1; row < rowCount; row++) {
-      for (let col = 1; col < colCount; col++) {
-        //DRAW THE LEFT AND BOTTOM LINES OF A GRID SQUARE - will draw the final top and right lines at the end
-        //let tR = 'grid[' + row + ',' + col + ']';
-        let tL = 'grid[' + row + ',' + (col - 1) + ']';
-        let bL = 'grid[' + (row - 1) + ',' + (col - 1) + ']';
-        let bR = 'grid[' + (row - 1) + ',' + col + ']';
-        //CREATE TOP/BOTTOM EDGES
-        let left = this.createEdge(this.getNodeByName(tL), this.getNodeByName(bL));
-        let bottom = this.createEdge(this.getNodeByName(bL), this.getNodeByName(bR));
-      }
-    }
-  
-    //ADD RIGHT-HAND EDGES
-    for (let row = 0; row < (rowCount-1); row++) {
-      let tR = 'grid[' + (row + 1) + ',' + (colCount - 1) + ']';
-      let bR = 'grid[' + (row) + ',' + (colCount - 1) + ']';
-      let right = this.createEdge(this.getNodeByName(tR), this.getNodeByName(bR));
-    }
-  
-    //ADD TOP EDGES
-    for (let col = 0; col < (colCount-1); col++) {
-      let tR = 'grid[' + (rowCount - 1) + ',' + (col + 1) + ']';
-      let tL = 'grid[' + (rowCount - 1) + ',' + col + ']';
-      let top = this.createEdge(this.getNodeByName(tR), this.getNodeByName(tL));
     }
   }
 }
