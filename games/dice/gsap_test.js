7ad8b6f0e02cd3566cdb72b9d8fbf0e00d3f5a62
eclectic-matt <eclecticappdevelopment@gmail.com>
2025-01-31 10:24:49 +0000

    more testing, stripping out joker slots logic

--- games/dice/gsap_test.js
+++ games/dice/gsap_test.js
@@ -1,5 +1,5 @@
 const jokers = document.querySelectorAll('.joker');
-
+const jokerRow = document.getElementById('jokerRow');
 let jokerOrder = [];
 
 jokers.forEach((item, i) => {
@@ -8,39 +8,55 @@ jokers.forEach((item, i) => {
 		
 		onPress:()=>{
 			//HIGHLIGHT SELECTED JOKER
-			gsap.to(item, {duration:0.1, scale:1.2, zIndex:100})
-			gsap.to(jokers, {duration:0.1, opacity:(i,t)=>(t==item)?1:0.3})
-			//REMOVE JOKER FROM CURRENT SLOT (AND REMOVE SLOT)
-			pickupJoker(item);
-		},
-
-		onDrag:()=>{
-			let jokerSlots = document.querySelectorAll('.jokerSlot');
-			if(Draggable.hitTest(item, jokerSlots, "20%")){
-				gsap.to(this.jokerSlots, {duration:0.1, scale: 1.2})
+			//gsap.to(item, {duration:0.1, scale:1.2, rotate:"10%", zIndex:100})
+			if (Draggable.hitTest(item, jokerRow, "20%")) {
+				removeJokerFromRow(jokerRow, item);
 			}
 		},
 
+		onDrag:()=>{
+			//if(Draggable.hitTest(item, jokerRow, "20%")){
+				//gsap.to(jokerRow, {duration:0.1, scale: 1.0})
+			//}
+			//gsap.to(item, {duration:0.1, rotate:"10%", zIndex:100})
+		},
+
 		onRelease:()=>{ // return the item on release
-			let slotFilled = false;
-			let jokerSlots = document.querySelectorAll('.jokerSlot');
-			gsap.to(item, {duration:0.4, scale:1, ease:'elastic.out(.45)'})
-			gsap.to(jokers, {duration:0.2, opacity:1, zIndex:0 })
+			//gsap.to(item, {duration:0.4, scale:1, rotate: 0})
+			//gsap.to(jokers, {duration:0.2, scale:1, opacity:1})
 			
+			let previousJoker = null;
+			//CHECK COLLISION WITH EXISTING JOKERS
+			Array.from(jokers).filter( (j) => {j.id !== item.id}).forEach( (joker) => {
+				//If the dropped joker overlaps another by 40%
+				if(Draggable.hitTest(item, joker, "40%")){
+					//set this joker as the "previous"
+					previousJoker = joker;
+				}
+			})
+			
+			if(Draggable.hitTest(item, jokerRow, "20%")){
+				console.log('bought joker', item.id, previousJoker);
+				appendJokerToRow(jokerRow, item, previousJoker);
+			}else{
+				removeJokerFromRow(jokerRow, item);
+			}
+			
+			//gsap.to(item, {duration:1, scale:1, transform:0, translate:0, rotate:0, position:"relative", x:jokerRow.x, y:jokerRow.y});
+			
+			//gsap.to(item, {duration:1, scale:1, transform:0, translate:0, rotate:0, x:jokerRow.x, y:jokerRow.y});
+			
+			//Draggable.get(item).kill();
+			/*
 			//find target
-			jokerSlots.forEach((slot, i) => {
+			if(Draggable.hitTest(item, jokerRow, "20%")){
 				
-				if(Draggable.hitTest(item, slot, "20%")){
-					slotFilled = true;
-					const newY = -slot.getBoundingClientRect().height;
-					const newX = slot.getBoundingClientRect().left + window.scrollX;
-					slot.dataset.joker = item.id;
-					setJokerInSlot(slot, item);
+				
 					const timeline = gsap.timeline({ defaults: { duration: 1 }})
 					timeline
 						.to(jokerSlots, {duration:0.1, opacity:(i,t)=>(t==slot)?1:0.3})
-						.to(slot, {duration:0.1, scale:1.2, zIndex:100})
-						.to(item, {x:newX, y:newY, position: 'absolute', zIndex: 10})
+						.to(slot, {duration:0.1, color: "#c33"})
+						//.to(item, {x:newX, y:newY, position: 'absolute'})
 						.to(jokerSlots, {duration:0.1, opacity:1, scale:1})
 				}
 			})
@@ -54,9 +70,77 @@ jokers.forEach((item, i) => {
 			}
 
 			updateJokerOrder();
+			*/
 		}
 	})
 })
+
+
+
+function appendJokerToRow(jokerRow, item, previousJoker=null){
+	let currentJokerCount = jokerRow.children.length;
+	//let jokerRow = document.getElementById('jokerRow');
+	//console.log(jokerRow);
+	if(previousJoker){
+		jokerRow.insertBefore(item, previousJoker);
+	}else{
+		jokerRow.appendChild(item);
+	}
+	var bounds = jokerRow.getBoundingClientRect();
+	var rect = item.getBoundingClientRect();
+	
+	let newX = bounds.left - rect.left + (currentJokerCount * 75);
+	//let newY = -(bounds.top - rect.top);
+	let newY = bounds.top;
+	gsap.to(item, {duration:1, x:newX, y:newY});
+	redrawJokerRow();
+}
+
+function removeJokerFromRow(jokerRow, item){
+	jokerRow.removeChild(item);
+	redrawJokerRow();
+}
+
+
+function redrawJokerRow(){
+	let jokerCount = jokerRow.children.length;
+	let centreX = Math.floor((jokerRow.offsetLeft + jokerRow.offsetWidth) / 2);
+	let centreY = Math.floor((jokerRow.offsetTop + jokerRow.offsetHeight) / 2);
+	let spreadX = 50;
+	Array.from(jokerRow.children).forEach( (joker) => {
+		//gsap.to(joker, {})
+		//joker.style.zIndex =
+		let jokerXPos = centreX - (spreadX * (jokerCount - 1));
+		gsap.to(joker, {x: jokerXPos})
+	})
+}
+
+
+
+
+
+function setJoker(slot, joker){
+	//APPEND JOKER AS CHILD OF SLOT,
+	//MOVING EXISTING JOKERS AS NEEDED
+	if(slot.children.length > 0){
+		let prev = slot.children[0];
+		console.log('prev', prev);
+		let newSlotId = addJokerSlot();
+		nextSlot = document.getElementById(newSlotId);
+		nextSlot.dataset.joker = slot.dataset.joker; 
+		nextSlot.appendChild(prev);
+		slot.removeChild(prev);
+	}
+	slot.appendChild(joker);
+}
+
+
+
+
+
+//=====
+// old
+//=====
 
 function pickupJoker(jokerId){
 	let jokerSlots = document.querySelectorAll('.jokerSlot');
@@ -71,10 +155,34 @@ function pickupJoker(jokerId){
 	});
 }
 
+function dropJoker(slotId, joker)
+{
+	let slot = document.getElementById(slotId);
+	//CHECK FOR EXISTING JOKER IN THIS SLOT
+	if(slot.dataset.joker){
+		let nextSlotIndex = slotId.replace('jokerSlot','');
+		let nextSlot = document.getElementById('jokerSlot' + (nextSlotIndex));
+		//THIS JOKER WILL BE PUSHED RIGHT - CHECK EXISTS
+		if(!nextSlot){
+			//CREATE SLOT AND ADD JOKER TO IT
+			let newSlotId = addJokerSlot();
+			nextSlot = document.getElementById(newSlotId);
+			nextSlot.dataset.joker = slot.dataset.joker;
+		}else{
+			nextSlot.dataset.joker = slot.dataset.joker;
+		}
+	}
+	slot.dataset.joker = joker.id;
+}
+function redrawJokers(){
+	
+}
+
 function removeElement(element) {
 	if (typeof(element) === "string") {
 		element = document.querySelector(element);
 	}
+	Draggable.get(element).kill();
 	return function() {
 		element.parentNode.removeChild(element);
 	};
@@ -158,8 +266,9 @@ function addJokerSlot(){
 	let newSlot = document.createElement('div');
 	newSlot.className = 'w3-col jokerSlot';
 	newSlot.id = 'jokerSlot' + (previousSlotCount + 1);
-	newSlot.innerHTML = newSlot.id;
+	//newSlot.innerHTML = newSlot.id;
 	jRow.appendChild(newSlot);
+	return newSlot.id;
 }
 
 function removeJokerSlot()
