9246142fe9f8df8b5dbe2f696be5d5bee50ef679
Matt <eclecticappdevelopment@gmail.com>
2025-03-04 17:08:35 +0000

    just storing some ideas

--- libraries/javascript/Maths.js
+++ libraries/javascript/Maths.js
@@ -32,4 +32,66 @@ class Maths
 	{
 		return Math.atan2(yb - ya, xb - xa) * 180 / Math.PI + 180;
 	}
+
+	matrixMultiply(matrixA, matrixB){
+		//CHECK DIMENSIONS ARE COMPATIBLE
+		if(matrixA.columns != matrixB.rows) return false;
+
+		
+	}
 }
+
+function Complex(real, imaginary) {
+	this.real = real;
+	this.imaginary = imaginary;
+  }
+  
+  Complex.prototype.print = function() {
+  
+	  let sign = '';
+  
+	  if (this.imaginary > 0){
+		  sign = '+';
+	  }
+  
+	return this.real + " " + sign + ' ' + this.imaginary + "i";
+  };
+  
+  Complex.prototype.add = function(c2){
+	  var r = this.real + c2.real;
+	  var i = this.imaginary + c2.imaginary;
+	  return new Complex(r, i);
+  }
+  
+  Complex.prototype.sub = function(c2){
+	  var r = this.real - c2.real;
+	  var i = this.imaginary - c2.imaginary;
+	  return new Complex(r,i);
+  }
+  
+  Complex.prototype.multiply = function(c2){
+	  var r = (this.real * c2.real) - (this.imaginary * c2.imaginary);
+	  var i = (this.real * c2.imaginary) + (c2.real * this.imaginary);
+	  return new Complex(r, i);
+  }
+  
+  Complex.prototype.divide = function(c2){
+	  var rA = (this.real * c2.real) + (this.imaginary * c2.imaginary);
+	  var i = (this.imaginary * c2.real) - (this.real * c2.imaginary);
+	  var rB = (c2.real * c2.real) + (c2.imaginary * c2.imaginary);
+	  return new Complex(rA / rB, i / rB);
+  }
+  
+  Complex.prototype.modulo = function(){
+	  var r = (this.real * this.real) + (this.imaginary * this.imaginary)
+	  return Math.sqrt(r);
+  }
+  
+  
+  /*
+   * Complex addition = (a+bi)+(c+di)=(a+c)+i(b+d),
+   * complex subtraction = (a+bi)-(c+di)=(a-c)+i(b-d)
+   * complex multiplication = (a+bi)(c+di)=(ac-bd)+i(ad+bc)
+   * complex division = (a+bi)/(c+di)=((ac+bd)+i(bc-ad))/(c^2+d^2)
+  */
+  
