adaf3aef7bc4e15baad92891fd47bac475e4c83e
Matt <eclecticappdevelopment@gmail.com>
2025-11-23 19:21:22 +0000

    update notes.js

--- board/botc/js/notes.js
+++ board/botc/js/notes.js
@@ -400,7 +400,7 @@ function createRolesWindow()
 		}
 		let teamHeaderLi = document.createElement('li');
 		teamHeaderLi.innerHTML = '<b>' + toTitleCase(team) + '</b>';
-		teamHeaderLi.style.fontSize = '1.5rem';
+		teamHeaderLi.style.fontSize = '1.35rem';
 		teamHeaderLi.style.listStyleType = 'none';
 		
 		
