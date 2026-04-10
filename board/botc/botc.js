4272d358de9edbe13eb112a97db5bac0d2d46120
Matt <eclecticappdevelopment@gmail.com>
2026-04-10 16:57:15 +0100

    added toString() for role count output

--- board/botc/botc.js
+++ board/botc/botc.js
@@ -355,7 +355,7 @@ function load(roles){
 					//ADD HEADER
 					//console.log('HEADER:',value.team);
 					let header3 = document.createElement('h3');
-					header3.innerHTML = value.team.toUpperCase() + ' (' + editions[edition].roles.length + ' roles)';
+					header3.innerHTML = value.team.toUpperCase() + ' (' + editions[edition].roles.length.toString() + ' roles)';
 					div.appendChild(header3);
 
 					var ul = document.createElement('ul');
