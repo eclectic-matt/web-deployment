f52a363d864a153939e50e87d9683923f88e2fd3
Matt <eclecticappdevelopment@gmail.com>
2026-05-06 08:21:12 +0100

    update game.js

--- board/coppershell/js/game.js
+++ board/coppershell/js/game.js
@@ -121,25 +121,3 @@ class Game
 		}
 	}
 }
-
-//SPLIT INTO UI SCRIPT
-showLocation = (locationName) => 
-{
-	//No location? Show town again
-	if(locationName === undefined)
-	{
-		//Reset location 
-	}
-	
-	//Show "back to town" button
-	let townBtn = document.getElementById("navBtnTown");
-	townBtn.innerHTML = "Back to town...";
-	townBtn.onclick = () => {
-		showLocation();
-	}
-	//Hide all locations
-	document.getElementById('town').style.display = 'none';
-	//Show current location large
-	let loc = document.getElementById('location');
-	loc.innerHTML = '<h2>SELECTED LOCATION</h2>';
-}
