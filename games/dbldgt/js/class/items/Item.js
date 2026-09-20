b33a0e36c5c946568d0603be09d233c235cfc14a
eclectic-matt <eclecticappdevelopment@gmail.com>
2026-09-20 17:40:07 +0100

    update Item.js

--- games/dbldgt/js/class/items/Item.js
+++ games/dbldgt/js/class/items/Item.js
@@ -1,57 +1,48 @@
 /**
  * Base class for all items, to be extended by different types.
  */
-class Item 
+class Item
 {
 	//Private fields
 	#id = "generic_item_000";
-	
-	//Fields that contribute to damage scoring
-	// - base
-	#baseAdd = 0;
-	#baseSub = 0;
-	#baseMul = 1;
-	#baseDiv = 1;
-	// - mult
-	#multAdd = 0;
-	#multSub = 0;
-	#multMul = 1;
-	#multDiv = 1;
-	// - special
-	//How many times this scores
-	#scoreTimes = 1;
-	
+	#type = "item";
 	//Public fields
-	category = "generic";
 	name = "item";
 	
-	constructor(category, name)
+	constructor(type, name)
 	{
-		this.category = category;
-		this.name = name;
 		this.#id = category + "_" + name + "_" + new Date().getMilliseconds();
+		this.#type = type;
+		this.name = name;
 	}
-	
+	/**
+	 * Getters and setters (private fields).
+ 	*/
+ 	//#id
 	get id()
 	{
 		return this.#id;
 	}
-	
-	score(damage)
+	set id(newId)
 	{
-		for(let i = 1; i <= this.#scoreTimes; i++)
-		{
-			//Apply base calculations
-			damage.base += this.#baseAdd;
-			damage.base -= this.#baseSub;
-			damage.base *= this.#baseMul;
-			damage.base /= this.#baseDiv;
-			//Apply mult calculations
-			damage.mult += this.#multAdd;
-			damage.mult -= this.#multSub;
-			damage.mult *= this.#multMul;
-			damage.mult /= this.#multDiv;
-		}
-		return damage;
+	  this.#id = newId;
+	}
+	//#type
+	get type()
+	{
+	  return this.#type;
+	}
+	set type(newType)
+	{
+	  this.#type = newType;
+	}
+	//name
+	get name()
+	{
+		return this.name;
+	}
+	set name(newName)
+	{
+		this.name = newName;
 	}
 }
